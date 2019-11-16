42886cbaf35f9ff53070a77358690a31aa2025d8
jurbelo <juho.kirvesmaki@selko.io>
2019-11-16 20:46:31 +0200

    frame for POC front part 1

--- stylizer-ui/src/App.js
+++ stylizer-ui/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import './App.css';
+/*
+filename: App.js 
+*/
+
+import React, { useCallback, useState } from "react";
+import cuid from "cuid";
+import ImageList from "./components/ImageList";
+import MyUploader from "./components/MyUploader";
+
+import "./App.css";
+
+function App() {
+
+  const [images, setImages] = useState([]);
+  // onDrop function  
+  const uploadModel = 'https://httpbin.org/post';
+  const uploadMe = 'https://httpbin.org/post';
 
 
-  class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        
-      };
-    }
-
-    render() {
+  // We pass onDrop function and accept prop to the component. It will be used as initial params for useDropzone hook
+  return (
+    <main className="App">
+      <h1 className="text-center">Put some clothes on!</h1>
       
-
-      return (
-        <div>
-          
-        </div>
-      );
-    }
-  }
+      <div className="App__uploaders">
+        <MyUploader className="App__uploader" upload= {uploadModel} text="Upload what you want to try on" />
+        <MyUploader className="App__uploader" upload={uploadMe} text="Upload a picture of yourself" />
+      </div>
+    </main>
+  );
+}
 
 export default App;
---
+++ stylizer-ui/src/components/MyUploader.js
@@ -0,0 +1,48 @@
+import React from "react";
+import 'react-dropzone-uploader/dist/styles.css'
+import Dropzone from 'react-dropzone-uploader'
+
+const MyUploader = (props) => {
+    // specify upload params and url for your files
+    const toast = (innerHTML) => {
+        const el = document.getElementById('toast')
+        el.innerHTML = innerHTML
+        el.className = 'show'
+        setTimeout(() => { el.className = el.className.replace('show', '') }, 3000)
+    }
+
+    const getUploadParams = () => {
+        return { url: props.upload }
+    }
+
+    const handleChangeStatus = ({ meta, remove }, status) => {
+        if (status === 'headers_received') {
+            toast(`${meta.name} uploaded!`)
+            remove()
+        } else if (status === 'aborted') {
+            toast(`${meta.name}, upload failed...`)
+        }
+    }
+
+    return (
+        <React.Fragment>
+
+            <Dropzone
+                getUploadParams={getUploadParams}
+                onChangeStatus={handleChangeStatus}
+                accept="image/*"
+                maxFiles={1}
+                multiple={false}
+                canCancel={false}
+                inputContent={props.text}
+                styles={{
+                    dropzone: { width: 200, height: 100 },
+                    dropzoneActive: { borderColor: 'green' },
+                }}
+            />
+            <div id="toast"></div>
+        </React.Fragment>
+    )
+}
+
+export default MyUploader
